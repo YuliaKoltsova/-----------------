400272254ae5a39e7c3c91157aa4a7a9ca68cc9d
Юлия Кольцова <yuliykoo@mail.ru>
2023-09-30 13:53:39 +0400

    Исправляет ф. редактирования задачи

--- js/add-notifications.js
+++ js/add-notifications.js
@@ -12,19 +12,11 @@ setInterval(() => {
   tasks.forEach((task) => {
     const dateTask = Date.parse(new Date(task.data + " " + task.time));
     // напоминание о ближайшей задачи будет за 10 минут до ее начала. 10 минут = 600000 миллисекунд
-    if (dateTask == currentDate - 600000 || dateTask <= currentDate -600000){ 
+    if (dateTask == currentDate + 600000 || dateTask <= currentDate + 600000){ 
       if (task.notification == false) {
-        notification.style.display = "flex";
-        notification.querySelector(".notification__name").textContent = task.name;
-        notification.querySelector(".notification__description").textContent = task.description;
-        notification.querySelector(".notification__date").textContent = task.data;
-        notification.querySelector(".notification__time").textContent = task.time;
+        alert("Напоминание! Через 10 минут:" + " " + task.name  + ". " + task.description + ". " +  task.data + " " + task.time);
         task.notification = true;
-        notificationCloseButton.addEventListener("click", () => {
-          notification.style.display = "none";
-        })
       }
     }
   })
-
 })
